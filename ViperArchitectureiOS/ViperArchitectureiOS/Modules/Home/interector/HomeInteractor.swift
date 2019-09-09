2a76482709006ec199b7fa79a1e4fb64d893b6f4
“Atif <atif.gcucs@gmail.com>
2019-09-09 12:30:32 +0400

    Viper Architecture
    Typical master detail example

--- ViperArchitectureiOS/ViperArchitectureiOS/Modules/Home/interector/HomeInteractor.swift
+++ ViperArchitectureiOS/ViperArchitectureiOS/Modules/Home/interector/HomeInteractor.swift
@@ -8,42 +8,20 @@
 
 import UIKit
 
-class HomeInteractor: HomePresenterToInteractorProtocol {
+class HomeInteractor: HomePresenterToInteractorProtocol, ServiceProtocol {
     var presenter: HomeInteractorToPresenterProtocol?
+    var service: Service?
     
     func fetchNews() {
-        // TODO Move boilerplate code
-        let session = URLSession.shared
-        let url = URL(string: Constants.request_url)!
-        var request = URLRequest(url: url)
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        session.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in
-            if let data = data {
-                do {
-                    if let json = try JSONSerialization.jsonObject(with: data) as? [String: Any] {
-                        if let status = json["status"] as? String, status == "ok" {
-                            if let content = json["articles"] as? [[String:String]] {
-                                var array = Array<NewsModel>()
-                                for item in content {
-                                    let decoder = JSONDecoder()
-                                    //decoder.dateDecodingStrategy = .iso8601
-                                    let jsonData = try JSONSerialization.data(withJSONObject: item)
-                                    //let jsonString = String(data: jsonData, encoding: .utf8)
-                                    let newsModel = try decoder.decode(NewsModel.self, from: jsonData)
-                                    //print(newsModel.author!)
-                                    array.append(newsModel)
-                                }
-                                self.presenter?.fetchNewsData(news: array)
-                            }
-                        }
-                    }
-                } catch {
-                    print(error)
-                    self.presenter?.fetchingNewsFailedWithError(error: error)
-                }
-            }
-        }).resume()
+        service = Service()
+        service?.delegate = self
+        service?.apiRequest()
     }
-    
+    func apiRespondSuccessfully(news: [NewsModel]) {
+        self.presenter?.fetchNewsData(news: news)
+    }
+    func apiRequestFailedWithError(error: Error) {
+        self.presenter?.fetchingNewsFailedWithError(error: error)
+    }
 }
 
---
+++ ViperArchitectureiOS/ViperArchitectureiOS/Service/Service.swift
@@ -0,0 +1,48 @@
+//
+//  Service.swift
+//  ViperArchitectureiOS
+//
+//  Created by Atif on 09/09/2019.
+//  Copyright © 2019 Atif. All rights reserved.
+//
+
+import UIKit
+
+protocol ServiceProtocol: class {
+    func apiRespondSuccessfully(news: [NewsModel])
+    func apiRequestFailedWithError(error: Error)
+}
+
+class Service: NSObject {
+    weak var delegate: ServiceProtocol?
+    func apiRequest() {
+        // TODO Move boilerplate code
+        let session = URLSession.shared
+        let url = URL(string: Constants.request_url)!
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        session.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in
+            if let data = data {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data) as? [String: Any] {
+                        if let status = json["status"] as? String, status == "ok" {
+                            if let content = json["articles"] as? [[String:String]] {
+                                var array = Array<NewsModel>()
+                                for item in content {
+                                    let decoder = JSONDecoder()
+                                    let jsonData = try JSONSerialization.data(withJSONObject: item)
+                                    let newsModel = try decoder.decode(NewsModel.self, from: jsonData)
+                                    array.append(newsModel)
+                                }
+                                self.delegate?.apiRespondSuccessfully(news: array)
+                            }
+                        }
+                    }
+                } catch {
+                    print(error)
+                    self.delegate?.apiRequestFailedWithError(error: error)
+                }
+            }
+        }).resume()
+    }
+}
